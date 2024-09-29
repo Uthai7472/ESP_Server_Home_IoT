4b4dc8d6847830b253c91fe143234adba8f7a570
Uthai7472 <uthai.khantamongkhon.a7p@ap.denso.com>
2024-09-29 15:22:03 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+let ledCommand = "OFF";
+let ledStatus = "";
+
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+app.use(bodyParser.json());
+
+// ---------------CONTROL ON/OFF LED------------------------------------
+app.post('/api/led-on-off', (req, res) => {
+    const {command} = req.body;
+
+    ledCommand = command;
+    console.log(ledCommand);
+
+    // Respond with success message
+    res.json({ message: `LED command updated to ${ledCommand}` });
+});
+
+app.get('/api/led-on-off', (req, res) => {
+    console.log(`Get request command: ${ledCommand}`);
+
+    res.json({ ledCommand });
+});
+
+// ---------------MONITOR ON/OFF LED------------------------------------
+app.post('/api/led-status', (req, res) => {
+    const {status} = req.body;
+
+    
+})
+
+app.listen(port, () => {
+    console.log(`Server in running on port: ${port}`);
+});
